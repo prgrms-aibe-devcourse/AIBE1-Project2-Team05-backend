90eb47a2c3dde6caa0ddb9dd5fc2f1350238aeea
soowan <nick6767676767@gmail.com>
2025-04-29 15:10:36 +0900

    feat: BaseEntity 정의

--- src/main/java/com/team05/linkup/domain/User.java
+++ src/main/java/com/team05/linkup/domain/User.java
@@ -1,5 +1,6 @@
 package com.team05.linkup.domain;
 
+import com.team05.linkup.domain.baseEntity.BaseEntity;
 import com.team05.linkup.domain.enums.ActivityTime;
 import com.team05.linkup.domain.enums.ActivityType;
 import com.team05.linkup.domain.enums.Interest;
@@ -16,7 +17,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(uniqueConstraints = @UniqueConstraint(name = "uk_provider_provider_id", columnNames = {"provider", "providerId"}))
-public class User {
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(length = 36, updatable = false, nullable = false)
