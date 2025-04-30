deb827081a8920db28d9e8545aa996fe11c184c8
usn757 <usn757@gmail.com>
2025-04-30 13:14:23 +0900

    feat: Enable Spring Data Web Support for Pageable and Page serialization

--- src/main/java/com/team05/linkup/LinkupApplication.java
+++ src/main/java/com/team05/linkup/LinkupApplication.java
@@ -3,9 +3,11 @@ package com.team05.linkup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class LinkupApplication {
 
     public static void main(String[] args) {
