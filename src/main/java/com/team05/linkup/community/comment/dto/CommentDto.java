f7fe03e63238643cc3ba641325084699864de1dd
23MinL <posing1606@gmail.com>
2025-04-30 10:24:31 +0900

    feat: Community 공통 DTO 클래스 구현

---
+++ src/main/java/com/team05/linkup/community/comment/dto/CommentDto.java
@@ -0,0 +1,52 @@
+package com.team05.linkup.community.comment.dto;
+
+import com.team05.linkup.domain.Comment;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CommentDto {
+
+    @Getter
+    @NoArgsConstructor
+    public static class Request {
+        private String commentContent;
+        private String parentCommentId; // 답글인 경우에만 사용
+    }
+
+    @Getter
+    @Builder
+    public static class Response {
+        private String id;
+        private String userId;
+        private String nickname;
+        private String profileImageUrl;
+        private String commentContent;
+        private boolean isParent;
+        private int totalLikeCount;
+        private ZonedDateTime createdAt;
+        private ZonedDateTime updatedAt;
+        private List<Response> childComments;
+
+        public static Response from(Comment comment, List<Comment> childComments) {
+            return Response.builder()
+                    .id(comment.getId())
+                    .userId(comment.getUser().getId())
+                    .nickname(comment.getUser().getNickname())
+                    .profileImageUrl(comment.getUser().getProfileImageUrl())
+                    .commentContent(comment.getCommentContent())
+                    .isParent(comment.isParent())
+                    .totalLikeCount(comment.getTotalLikeCount())
+                    .createdAt(comment.getCreatedAt())
+                    .updatedAt(comment.getUpdatedAt())
+                    .childComments(childComments.stream()
+                            .map(child -> Response.from(child, List.of()))
+                            .collect(Collectors.toList()))
+                    .build();
+        }
+    }
+}
--- src/main/java/com/team05/linkup/domain/enums/CommunityCategory.java
+++ src/main/java/com/team05/linkup/domain/enums/CommunityCategory.java
@@ -22,4 +22,5 @@ public enum CommunityCategory {
 //        }
 //        throw new IllegalArgumentException("Invalid category name: " + name);
 //    }
+
 }
