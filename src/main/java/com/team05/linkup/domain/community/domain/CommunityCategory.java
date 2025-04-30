0342e9aa4c302323a180bb460f4c5aa920393f6d
usn757 <usn757@gmail.com>
2025-04-30 12:52:40 +0900

    feat: Add CommunityCategory enum

---
+++ src/main/java/com/team05/linkup/domain/community/domain/CommunityCategory.java
@@ -0,0 +1,14 @@
+package com.team05.linkup.domain.community.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommunityCategory {
+    QUESTION, // ("질문/답변"), // Keep korean names if needed for display elsewhere
+    INFO,     // ("정보공유"),
+    REVIEW,   // ("후기"),
+    FREE,     // ("자유게시판"),
+    TALENT;   // ("재능나눔");
+}
