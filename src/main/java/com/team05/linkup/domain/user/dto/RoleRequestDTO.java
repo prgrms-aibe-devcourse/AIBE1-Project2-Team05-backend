614abc8d75f2067d88e8aef22f98c7e30eedc2ae
soowan <nick6767676767@gmail.com>
2025-04-30 12:48:29 +0900

    feat: 권한 변경 기능

--- src/main/java/com/team05/linkup/domain/user/dto/RoleRequestDTO.java
+++ src/main/java/com/team05/linkup/domain/user/dto/RoleRequestDTO.java
@@ -1,6 +1,10 @@
 package com.team05.linkup.domain.user.dto;
 
 import com.team05.linkup.domain.enums.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record RoleRequestDTO(Role role) {
+@Schema(description = "사용자 권한을 담는 DTO")
+public record RoleRequestDTO(
+        @Schema(description = "권한", example = "ROLE_MENTOR")
+        Role role) {
 }
