bacef0031e636de72cf95305cc4a458855a9bba6
taehyun <kimtaehyun002@gmail.com>
2025-05-01 20:06:03 +0900

    feat: 회원 페이지| 회원 프로필 조회 구현 (멘토와 멘티 공통 정보)

--- src/main/java/com/team05/linkup/domain/user/api/ProfileController.java
+++ src/main/java/com/team05/linkup/domain/user/api/ProfileController.java
@@ -8,6 +8,7 @@ import com.team05.linkup.domain.enums.Role;
 import com.team05.linkup.domain.user.application.MenteeProfileService;
 import com.team05.linkup.domain.user.application.MentorProfileService;
 import com.team05.linkup.domain.user.application.ProfileService;
+import com.team05.linkup.domain.user.dto.ProfilePageDTO;
 import com.team05.linkup.domain.user.infrastructure.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -35,8 +36,23 @@ public class ProfileController {
     private final MentorProfileService mentorProfileService;
     private final MenteeProfileService menteeProfileService;
 
-    @GetMapping("/{nickname}/profile/activity")
-    public ResponseEntity<ApiResponse> getProfile(@PathVariable String nickname) {
+    @GetMapping("/{nickname}")
+    public ResponseEntity<ApiResponse<ProfilePageDTO>> getProfile(@PathVariable String nickname) {
+
+        Optional<User> userOpt = userRepository.findByNickname(nickname);
+        if (userOpt.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ApiResponse.error(ResponseCode.ENTITY_NOT_FOUND, "프로필을 찾을 수 없습니다."));
+
+
+        ProfilePageDTO profilePageDTO = profileService.getProfile(userOpt.get());
+
+        return ResponseEntity.ok(ApiResponse.success(profilePageDTO));
+    }
+
+
+    @GetMapping("/{nickname}/activity")
+    public ResponseEntity<ApiResponse> getActivity(@PathVariable String nickname) {
         // 1. 사용자의 역할(멘토/멘티) 확인
         Optional<User> userOpt = userRepository.findByNickname(nickname);
         if (userOpt.isEmpty())
