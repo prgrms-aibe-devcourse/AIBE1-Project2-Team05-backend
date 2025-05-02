7704108f7502779d1a6572464c33ad1666e42417
soowan <nick6767676767@gmail.com>
2025-05-02 13:02:21 +0900

    refactor: @AuthenticationPrincipal 어노테이션 사용으로 변경

--- src/main/java/com/team05/linkup/domain/mentoring/api/AiMatchingController.java
+++ src/main/java/com/team05/linkup/domain/mentoring/api/AiMatchingController.java
@@ -7,13 +7,13 @@ import com.team05.linkup.common.util.JwtUtils;
 import com.team05.linkup.domain.mentoring.dto.AiMatchingResponseDTO;
 import com.team05.linkup.domain.mentoring.service.AiMatchingListServiceImpl;
 import com.team05.linkup.domain.mentoring.service.AiMatchingSelectorServiceImpl;
-import io.jsonwebtoken.Claims;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,21 +28,20 @@ public class AiMatchingController {
     @GetMapping("/recommendation")
     @Operation(description = "ai 매칭 멘토 리스트 결과")
 
-    public ResponseEntity<ApiResponse<AiMatchingResponseDTO>> getRecommendation(HttpServletRequest request) {
+    public ResponseEntity<ApiResponse<AiMatchingResponseDTO>> getRecommendation(HttpServletRequest request,
+                                                                                @AuthenticationPrincipal String providerId,
+                                                                                @AuthenticationPrincipal String provider) {
         try {
-            String token = jwtUtils.extractToken(request);
-            if (token != null && !token.isEmpty()) {
-                boolean isValid = jwtUtils.validateToken(token);
-                if (isValid) {
-                    Claims claims = jwtUtils.parseToken(token);
-                    String providerId = claims.getSubject();
-                    String provider = (String) claims.get("provider");
-                    AiMatchingResponseDTO responseDTO = aiMatchingServiceImpl.matchMentor(provider,providerId);
-                    return ResponseEntity.ok(ApiResponse.success(responseDTO));
+                if (providerId == null || provider == null) {
+                    return ResponseEntity
+                            .status(ResponseCode.UNAUTHORIZED.getStatus())
+                            .body(ApiResponse.error(ResponseCode.UNAUTHORIZED));
                 }
-            }
-            return ResponseEntity.ok(ApiResponse.error(ResponseCode.UNAUTHORIZED));
-        } catch (Exception e) {
+                AiMatchingResponseDTO responseDTO = aiMatchingServiceImpl.matchMentor(provider,providerId);
+                return ResponseEntity.ok(ApiResponse.success(responseDTO));
+
+        }
+        catch (Exception e) {
             return ResponseEntity.ok(ApiResponse.error(ResponseCode.INTERNAL_SERVER_ERROR));
         }
     }
@@ -50,20 +49,18 @@ public class AiMatchingController {
     @PostMapping("/{nickname}")
     @Operation(description = "ai 매칭 멘토 선택")
     @PreAuthorize("hasAuthority('ROLE_MENTEE')")
-    public ResponseEntity<ApiResponse> matchMentor(HttpServletRequest request, @PathVariable String nickname) {
+    public ResponseEntity<ApiResponse> matchMentor(HttpServletRequest request,
+                                                   @PathVariable String nickname,
+                                                   @AuthenticationPrincipal String providerId,
+                                                   @AuthenticationPrincipal String provider) {
         try {
-            String token = jwtUtils.extractToken(request);
-            if (token != null && !token.isEmpty()) {
-                boolean valid = jwtUtils.validateToken(token);
-                if (valid) {
-                    Claims claims = jwtUtils.parseToken(token);
-                    String providerId = claims.getSubject();
-                    String provider = (String) claims.get("provider");
+            if (providerId == null || provider == null) {
+                return ResponseEntity
+                        .status(ResponseCode.UNAUTHORIZED.getStatus())
+                        .body(ApiResponse.error(ResponseCode.UNAUTHORIZED));
+                }
                     aiMatchingSelectorServiceImpl.matchingMentor(provider, providerId, nickname);
                     return ResponseEntity.ok(ApiResponse.success());
-                }
-            }
-            return ResponseEntity.ok(ApiResponse.error(ResponseCode.UNAUTHORIZED));
         } catch (DuplicateMentoringMatchException e) {
             return ResponseEntity.ok(ApiResponse.error(ResponseCode.INVALID_INPUT_VALUE));
         }
