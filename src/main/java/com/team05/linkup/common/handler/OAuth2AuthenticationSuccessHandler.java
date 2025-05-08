275f1ec58e18bd75325da296ff2aa709dad419af
soowan <nick6767676767@gmail.com>
2025-05-08 17:36:55 +0900

    fix: 프론트 경로 변경

--- src/main/java/com/team05/linkup/common/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/team05/linkup/common/handler/OAuth2AuthenticationSuccessHandler.java
@@ -71,7 +71,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
             response.setHeader("Set-Cookie", cookie.toString());
 
             // 리디렉션 URL 생성
-            String redirectUrl = String.format("https://frontend.llinkup.o-r.kr/user-type-selection?loggedIn=%s&socialType=%s",
+            String redirectUrl = String.format("https://frontend.linkup.o-r.kr/user-type-selection?loggedIn=%s&socialType=%s",
                     true, provider);
 
             logger.info("Redirecting to: {}", redirectUrl);
