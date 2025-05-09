901a557676076ece4923b5f1f95a4593068c7542
soowan <nick6767676767@gmail.com>
2025-05-09 21:54:12 +0900

    fix: 쿠키 제대로 지워주기

--- src/main/java/com/team05/linkup/common/handler/CustomLogoutSuccessHandler.java
+++ src/main/java/com/team05/linkup/common/handler/CustomLogoutSuccessHandler.java
@@ -46,7 +46,7 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
                     }
                 }
             }
-            ResponseCookie cookie = ResponseCookie.from("jwt_token", null)
+            ResponseCookie cookie = ResponseCookie.from("jwt_token", "")
                     .sameSite("None")
                     .secure(true)
                     .httpOnly(true)
@@ -54,7 +54,6 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
                     .domain(".linkup.o-r.kr")
                     .path("/")
                     .build();
-
             response.setStatus(HttpServletResponse.SC_OK);
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF-8");
